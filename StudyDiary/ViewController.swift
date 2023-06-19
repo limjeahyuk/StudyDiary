4e7628a1c17ae23ad42c005ce9e4eb8d71fe35c0
limjeahyuk <lim0202jh@gmail.com>
2023-06-20 00:49:12 +0900

    Initial Commit

---
+++ StudyDiary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StudyDiary
+//
+//  Created by 임재혁 on 2023/06/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
